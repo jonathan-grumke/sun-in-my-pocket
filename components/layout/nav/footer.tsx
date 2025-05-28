8ae1524779eba942c3f582ea84a4096e82ca1211
jonathan-grumke <grumkejonathan@yahoo.de>
2025-05-29 09:21:30 +1000

    migrate header and footer

--- components/layout/nav/footer.tsx
+++ components/layout/nav/footer.tsx
@@ -1,38 +1,21 @@
-"use client";
-import React from "react";
-import Link from "next/link";
-import { Icon } from "../../icon";
-import { useLayout } from "../layout-context";
+'use client';
+import React from 'react';
+import { SocialLinks } from '@/components/social-links';
 
 export const Footer = () => {
-  const { globalSettings } = useLayout();
-  const { header, footer } = globalSettings!;
+  const today = new Date();
 
   return (
-    <footer className="border-b bg-white pt-20 dark:bg-transparent">
-      <div className="mx-auto max-w-5xl px-6">
-        <div className="mt-12 flex flex-wrap items-center gap-6 border-t py-6 flex-col md:flex-row md:justify-between">
-
-          <div className="order-last flex justify-center md:order-first md:justify-start">
-            <Link href="/" aria-label="go home">
-              <Icon
-                parentColor={header!.color!}
-                data={header!.icon}
-              />
-            </Link>
-            <span className="self-center text-muted-foreground text-sm ml-2">© {new Date().getFullYear()} {header?.name}, All rights reserved</span>
-          </div>
-
-          <div className="order-first flex justify-center gap-6 text-sm md:order-last md:justify-end">
-            {footer?.social?.map((link, index) => (
-              <Link key={`${link!.icon}${index}`} href={link!.url!} target="_blank" rel="noopener noreferrer" >
-                <Icon data={{ ...link!.icon, size: 'small' }} className="text-muted-foreground hover:text-primary block" />
-              </Link>
-            ))}
-          </div>
-
-        </div>
+    <footer>
+      <div className='footer-navigation'>
+        <a href='/contact'> Kontakt </a>
+        <a href='/imprint'> Impressum </a>|<a href='/'>Home</a>
+        <a href='/travel-diary'>Reisetagebuch</a>
+        <a href='/travel-guide'>Reiseziele</a>
+        <a href='/about'>Über uns</a>
       </div>
+      &copy; {today.getFullYear()} Sun in my pocket. All rights reserved.
+      <SocialLinks />
     </footer>
   );
-}
+};
