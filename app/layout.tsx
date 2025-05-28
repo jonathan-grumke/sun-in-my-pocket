2a51621d857fbe0c57774553b21ce41514ffbab3
jonathan-grumke <grumkejonathan@yahoo.de>
2025-05-28 12:02:30 +1000

    comment out tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,34 @@
-import React from "react";
-import { Metadata } from "next";
-import { Inter as FontSans, Lato, Nunito } from "next/font/google";
-import { cn } from "@/lib/utils";
-import { VideoDialogProvider } from "@/components/ui/VideoDialogContext";
-import VideoDialog from "@/components/ui/VideoDialog";
+import React from 'react';
+import { Metadata } from 'next';
+import { Inter as FontSans, Lato, Nunito } from 'next/font/google';
+import { cn } from '@/lib/utils';
+import { VideoDialogProvider } from '@/components/ui/VideoDialogContext';
+import VideoDialog from '@/components/ui/VideoDialog';
 
-import "@/styles.css";
-import { TailwindIndicator } from "@/components/ui/breakpoint-indicator";
+// import '@/styles.css';
+// import { TailwindIndicator } from '@/components/ui/breakpoint-indicator';
+
+import '@/styles/global.css';
 
 const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
+  subsets: ['latin'],
+  variable: '--font-sans',
 });
 
 const nunito = Nunito({
-  subsets: ["latin"],
-  variable: "--font-nunito",
+  subsets: ['latin'],
+  variable: '--font-nunito',
 });
 
 const lato = Lato({
-  subsets: ["latin"],
-  variable: "--font-lato",
-  weight: "400",
+  subsets: ['latin'],
+  variable: '--font-lato',
+  weight: '400',
 });
 
 export const metadata: Metadata = {
-  title: "Tina",
-  description: "Tina Cloud Starter",
+  title: 'Tina',
+  description: 'Tina Cloud Starter',
 };
 
 export default function RootLayout({
@@ -35,13 +37,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={cn(fontSans.variable, nunito.variable, lato.variable)}>
-      <body className="min-h-screen bg-background font-sans antialiased">
+    <html lang='en' className={cn(fontSans.variable, nunito.variable, lato.variable)}>
+      <body className='min-h-screen bg-background font-sans antialiased'>
         <VideoDialogProvider>
           {children}
           <VideoDialog />
         </VideoDialogProvider>
-        <TailwindIndicator />
+        {/* <TailwindIndicator /> */}
       </body>
     </html>
   );
