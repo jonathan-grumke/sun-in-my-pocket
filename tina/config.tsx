1d1ac6743344cce37322d998a7508edaa034609d
jonathan-grumke <grumkejonathan@yahoo.de>
2025-06-06 18:38:25 -1000

    change tina branch to preview

--- tina/config.tsx
+++ tina/config.tsx
@@ -10,10 +10,9 @@ import TravelDiary from './collection/travel-diary';
 
 const config = defineConfig({
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
-  branch:
-    process.env.NEXT_PUBLIC_TINA_BRANCH! || // custom branch env override
-    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
-    process.env.HEAD!, // Netlify branch env
+  branch: 'preview',
+  // process.env.NEXT_PUBLIC_TINA_BRANCH! || // custom branch env override
+  // process.env.HEAD!, // Netlify branch env
   token: process.env.TINA_TOKEN!,
   media: {
     // If you wanted cloudinary do this
