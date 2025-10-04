2ed72ddc670187aced6b6f02cb28fd2bfe54a314
jonathan-grumke <grumkejonathan@yahoo.de>
2025-10-04 23:04:44 +0200

    add rss feed

---
+++ app/feed.xml/route.ts
@@ -0,0 +1,38 @@
+import RSS from "rss";
+import client from "@/tina/__generated__/client";
+
+const feed = new RSS({
+  title: "Sun In My Pocket",
+  description: "Folge uns auf unseren Reisen rund um die Welt.",
+  site_url: "https://suninmypocket.com",
+  feed_url: `https://suninmypocket.com/feed.xml`,
+  copyright: `${new Date().getFullYear()} Your Website`,
+  language: "de-DE",
+  pubDate: new Date(),
+});
+
+const posts = await client.queries.travelDiaryConnection({});
+
+posts.data.travelDiaryConnection.edges?.forEach((postData) => {
+  const post = postData!.node!;
+  feed.item({
+    title: post.title,
+    guid: `https://suninmypocket.com/travel-diary/${post!._sys.breadcrumbs.join(
+      "/"
+    )}`,
+    url: `https://suninmypocket.com/travel-diary/${post!._sys.breadcrumbs.join(
+      "/"
+    )}`,
+    date: post.date,
+    description: post.description,
+    author: post.author,
+  });
+});
+
+export async function GET() {
+  return new Response(feed.xml({ indent: true }), {
+    headers: {
+      "Content-Type": "application/atom+xml; charset=utf-8",
+    },
+  });
+}
